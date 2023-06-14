7574052b2878e668de074cd038f0b938e9683533
Divyanshu Rana <104566386+divyansh453@users.noreply.github.com>
2023-06-14 23:49:24 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
-EMAIL_HOST_USER = 'divyanshurana312@gmail.com'
-EMAIL_HOST_PASSWORD ='agclmljzenaeekht'
+EMAIL_HOST_USER = #Enter your email 
+EMAIL_HOST_PASSWORD =#Enter password
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
